305a29e3195240d07b6aca01cdf4c0d8c402095a
Nuthan B <64555950+NuthanB@users.noreply.github.com>
2023-12-29 13:22:40 +0530

    searchpage ui (#70)

--- app/src/Pages/SearchDetailsShow/SearchDetailsShow.js
+++ app/src/Pages/SearchDetailsShow/SearchDetailsShow.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from 'react';
 import { useLocation, useParams } from "react-router-dom";
 import TextNameComponent from "../../Components/TextNameComponent";
 import "./SearchDetails.css";
@@ -8,12 +9,26 @@ const SearchDetailsShow = () => {
   const location = useLocation();
   const { resultData } = location.state || {};
   const who = location.state?.who || " ";
+  const [isChecked1, setIsChecked1] = useState(false);
+  const [isChecked2, setIsChecked2] = useState(false);
+  const months = [
+    "January", "February", "March", "April", "May", "June",
+  ];
+
+  const handleToggle1 = () => {
+    setIsChecked1(!isChecked1);
+  };
+
+  const handleToggle2 = () => {
+    setIsChecked2(!isChecked2);
+  };
 
   console.log(resultData);
 
   return (
     <div>
       {resultData && (
+        <div>
         <div className="search-wrapper-details">
           <div className="search-container-details">
             <div className="search-wrapper">
@@ -79,7 +94,82 @@ const SearchDetailsShow = () => {
               />
             )}
           </div>
+          <div>
+          </div>
         </div>
+      <div className="Fees">
+       <div>
+        <div className="fees-section">
+          <div className="fees-container">
+          <h3 className="fees-text">Application Fees</h3><button className="option-button">Receipt</button>
+          </div>
+          <div className="fees-container">
+          <h3 className="fees-text-2">Admission Fees</h3><button className="pay-button ">Take Fees</button>
+          </div>
+        </div>
+       </div>
+       <div className="fees-section">
+        <div className="fees-container">
+              <h2 className="Regular-Fees">Regular Fees</h2>
+              <p className="mode">Annually</p>
+              <label className="toggle-switch">
+                <input type="checkbox" checked={isChecked1} onChange={handleToggle1} />
+                <span className="slider round"></span>
+                </label>
+              <p className="mode">Monthly</p>
+              <hr></hr>
+        </div>
+        <div>
+          <br></br>
+          <hr></hr>
+          <div className="Month">
+
+          </div>
+        </div>
+       </div>
+       <div className="fees-section">
+        <div className="fees-container">
+              <h2 className="Regular-Fees">Transport Fees</h2>
+              <p className="mode">Annually</p>
+              <label className="toggle-switch">
+                <input type="checkbox" checked={isChecked2} onChange={handleToggle2} />
+                <span className="slider round"></span>
+                </label>
+              <p className="mode">Monthly</p>
+              <hr></hr>
+        </div>
+        <div>
+          <br></br>
+          <hr></hr>
+        </div>
+       </div>
+    </div>
+  <div className="marks-section">
+      <div className="marks-container">
+        <div className="marks-container">
+          <h3 className="downloads-1">Marks Card</h3><button className="option-button">Download</button>
+        </div>
+        <div className="marks-container">
+          <h3 className="downloads-1">Hall Ticket</h3><button className="option-button">Download</button>
+        </div>
+      </div>
+    <div className="marks-container">
+        <div className="marks-container">
+          <h3 className="downloads">Previous year Data</h3><button className="option-button">Download</button>
+          
+        </div>
+        <div className="marks-container">
+          <h3 className="downloads">No Due Certificate</h3><button className="option-button">Download</button>
+        </div>
+    </div>
+  </div>
+  <div className="ExamAttendance">
+      <div className="exam-section">
+          <h3 className="exam">Exam wise Grades</h3>
+            <hr></hr>
+      </div>
+   </div> 
+      </div>
       )}
     </div>
   );
