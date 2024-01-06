2d4539f3d9ebd94fbbe96c2ee8f526c54c37ee61
Nuthan B <NuthanB@protonmail.com>
2024-01-06 12:02:15 +0530

    added the graph navbar left

--- app/src/Pages/VisualizeGraphs/VisualizeGraphs.js
+++ app/src/Pages/VisualizeGraphs/VisualizeGraphs.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+import BarGraph from "../../Components/BarGraph";
+import BarGraphData from "../../Database/BarGraphData";
 
 const VisualizeGraphs = () => {
+  const [attendanceBarGraphData, setAttendanceBarGraphData] = useState({
+    labels: BarGraphData.map((data) => data.class),
+    datasets: [
+      {
+        label: "Student Attendance",
+        data: BarGraphData.map((data) => data.attendance),
+        backgroundColor: ["red", "#ecf0f1", "#50AF95", "#F3ba2f", "#2a71d0"],
+        borderColor: "black",
+        borderWidth: 1,
+      },
+    ],
+  });
   return (
-    <div>VisualizeGraphs</div>
+    <div className="w-3/4">
+        <BarGraph data={attendanceBarGraphData} />
+      </div>
   )
 }
 
